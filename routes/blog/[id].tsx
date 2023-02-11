c6b33afe3a9c2b62f5586dc40cb299c41f925f27
Speedev <roberto.tg99@gmail.com>
2023-02-10 23:46:26 -0500

    feat: Add layout in pages

--- routes/blog/[id].tsx
+++ routes/blog/[id].tsx
@@ -1,14 +1,12 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
 import { loadPost } from "../../utils/posts.ts";
 import { CSS } from "$gfm/mod.ts";
-import Navbar from "../../components/Navbar/Navbar.tsx";
+import Layout from "../../components/Layout/Layout.tsx";
 
 export const handler: Handlers = {
   async GET(request, context) {
     const { id } = context.params;
     const post = await loadPost(id);
-    console.log(post);
-
     return context.render({ post });
   },
 };
@@ -16,24 +14,25 @@ function PagePost(props: PageProps) {
   const { post } = props?.data || {};
   return (
     <div class="mt-2 p-4">
-      <Navbar />
-      <div class="flex justify-center px-0">
-        <article class="w-[48rem]">
-          <header class="mb-10">
-            <h1 class="text-3xl font-bold">{post.title}</h1>
-            <time class="text-blue-600 text-sm">
-              {Intl.DateTimeFormat("es", { dateStyle: "long" }).format(
-                post.date,
-              )}
-            </time>
-          </header>
-          <style dangerouslySetInnerHTML={{ __html: CSS }} />
-          <div
-            class="markdown-body"
-            dangerouslySetInnerHTML={{ __html: post.body }}
-          />
-        </article>
-      </div>
+      <Layout>
+        <div class="flex justify-center px-0">
+          <article class="w-[48rem]">
+            <header class="mb-10">
+              <h1 class="text-3xl font-bold">{post.title}</h1>
+              <time class="text-blue-600 text-sm">
+                {Intl.DateTimeFormat("es", { dateStyle: "long" }).format(
+                  post.date,
+                )}
+              </time>
+            </header>
+            <style dangerouslySetInnerHTML={{ __html: CSS }} />
+            <div
+              class="markdown-body"
+              dangerouslySetInnerHTML={{ __html: post.body }}
+            />
+          </article>
+        </div>
+      </Layout>
     </div>
   );
 }
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,7 +1,9 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
 import { Post } from "../types.d.ts";
 import { listPosts } from "../utils/posts.ts";
-import Navbar from "../components/Navbar/Navbar.tsx";
+// import Navbar from "../components/Navbar/Navbar.tsx";
+import Layout from "../components/Layout/Layout.tsx";
+import Skill from "../components/Skill/Skill.tsx";
 
 export const handler: Handlers = {
   async GET(req, context) {
@@ -13,10 +15,10 @@ export default function Home(props: PageProps) {
   const { data } = props;
   const { posts } = data;
   return (
-    <div class="md:mx-24 lg:mx-24 mt-2 p-4">
-      <Navbar />
-      <main class="grid lg:grid-cols-4 lg:grid-rows-4 lg:grid-flow-col gap-4">
-        <aside class="row-span-4">
+    <div class="md:mx-24 lg:mx-24 mt-2 p-2">
+      <Layout>
+      <main class="grid lg:grid-cols-4 lg:grid-rows-4 lg:grid-flow-col gap-4 min-h-full">
+        <aside class="row-span-4 p-2">
           <h1 class="text-4xl font-bold text-blue-600">
             Roberto
             <br />
@@ -32,33 +34,18 @@ export default function Home(props: PageProps) {
           </p>
         </aside>
 
-        <section class="col-span-3">
-          <h2 class="text-xl font-bold text-blue-600 px-3">Skills</h2>
-          <div class="grid grid-flow-col py-2 px-3">
-            <img class="w-12" src="/github-svgrepo-com.svg" alt="css-logo" />
-            <img class="w-12" src="/html-svgrepo-com.svg" alt="css-logo" />
-            <img class="w-12" src="/css-svgrepo-com.svg" alt="css-logo" />
-            <img class="w-12" src="/go-svgrepo-com.svg" alt="css-logo" />
-            <img class="w-12" src="/java-svgrepo-com.svg" alt="css-logo" />
-            <img class="w-12" src="/python-svgrepo-com.svg" alt="css-logo" />
-            <img
-              class="w-12"
-              src="/javascript-svgrepo-com.svg"
-              alt="css-logo"
-            />
-            <img class="w-12" src="/deno-svgrepo-com.svg" alt="css-logo" />
-            <img class="w-12" src="/react-svgrepo-com.svg" alt="css-logo" />
-            <img class="w-12" src="/vue-svgrepo-com.svg" alt="css-logo" />
-          </div>
-        </section>
+          <section class="col-span-3">
+            <h2 class="text-xl font-bold text-blue-600 p-2">Skills</h2>
+            <Skill />
+          </section>
 
         <section class="row-span-2 col-span-2">
-          <h2 class="text-xl font-bold px-3 text-blue-600">
+          <h2 class="text-xl font-bold text-blue-600 p-2">
             Últimos articulos
           </h2>
           {posts.map((post: Post) => (
             <a href={`/blog/${post.id}`}>
-              <article class="rounded-lg hover:bg-blue-50 py-2 px-3">
+              <article class="rounded-lg hover:bg-blue-50 py-2 px-3 w-full">
                 <h2 class="text-xl font-medium">{post.title}</h2>
                 <time class="text-blue-600 text-sm">
                   {Intl.DateTimeFormat("es", { dateStyle: "long" }).format(
@@ -72,7 +59,7 @@ export default function Home(props: PageProps) {
         {
           /*
         <section class="row-span-2 col-span-3">
-          <h2 class="text-xl font-bold text-red-900 px-3">
+          <h2 class="text-xl font-bold text-red-900 px-2">
             Últimos proyectos
           </h2>
           <a href="">
@@ -86,7 +73,7 @@ export default function Home(props: PageProps) {
         }
       </main>
 
-      <footer></footer>
+      </Layout>
     </div>
   );
 }
