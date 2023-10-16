2467f26812bd0b5c7b50a7bfb7c8aca5e8b43e5c
Speedev <roberto.tg99@gmail.com>
2023-10-16 10:25:30 -0500

    :lipstick: Add property style hyphens for markdown

--- components/Markdown/Markdown.tsx
+++ components/Markdown/Markdown.tsx
@@ -6,7 +6,7 @@ interface Props {
 const Markdown: FunctionComponent<Props> = ({ body }) => (
   <div>
     <div
-      style="text-align: justify"
+      style="text-align: justify; hyphens: auto"
       className="prose markdown-body dark:text-white"
       dangerouslySetInnerHTML={{ __html: body }}
     />
