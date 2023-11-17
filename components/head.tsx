9110a27cb2d7c700aeab36b314425fe98923e044
roberto-carlos-tg <roberto.tg99@gmail.com>
2023-11-16 19:38:44 -0500

    :art: Format code, and change title name

--- components/head.tsx
+++ components/head.tsx
@@ -1,34 +1,44 @@
-import { Head } from '$fresh/runtime.ts'
-
+import { Head } from "$fresh/runtime.ts";
 
 const HeadTag = () => {
-  const desc = "Artículos, recursos y tutoriales sobre Desarrollo Web, Linux, Frontend, JavaScript ☕️, React ⚛️, CSS 🎨 y Performance Web ⚡️"
-  const defaultTitle = 'Roberto Toalongto 💻 | Frontend, JavaScript, React, CSS, Performance'
-  const defaultOgType = 'website'
+  const desc =
+    "Artículos, recursos y tutoriales sobre Desarrollo Web, Linux, Frontend, JavaScript ☕️, React ⚛️, CSS 🎨 y Performance Web ⚡️";
+  const defaultTitle =
+    "Berht 💻 | Frontend, JavaScript, React, CSS, Performance";
+  const defaultOgType = "website";
 
   return (
     <Head>
       <title>{defaultTitle}</title>
-      <meta name='description' content={desc} />
-      <meta name='author' content='Roberto Toalongo' />
+      <meta name="description" content={desc} />
+      <meta name="author" content="Roberto Toalongo" />
       <meta
-        name='keywords'
-        content='Frontend, Javascript, Pyton, React, CSS, HTML'
+        name="keywords"
+        content="Frontend, Javascript, Pyton, React, CSS, HTML"
       />
-      <meta
-        property='og:description'
-        content={desc}
+      <meta property="og:description" content={desc} />
+      <meta property="og:type" content={defaultOgType} />
+      <link
+        rel="icon"
+        href="/favicon/favicon.ico"
+        type="image/x-icon"
+        sizes="96x96"
       />
-      <meta property='og:type' content={defaultOgType} />
-      <link rel="icon" href="/favicon/favicon.ico" type="image/x-icon" sizes="16x16" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-icon-180x180.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicon/apple-icon-180x180.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon/favicon-96x96.png"
+      />
       <link rel="stylesheet" href="/styles/styles.css" />
-      <script id='theme' src='/scripts/theme.js' />
+      <script id="theme" src="/scripts/theme.js" />
     </Head>
-  )
-}
+  );
+};
 
-export default HeadTag
-
-
+export default HeadTag;
